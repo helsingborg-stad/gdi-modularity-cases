ae3533de6879cc89c461fdc32520949d59adb539
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-12-14 10:15:34 +0100

    formatting

--- source/js/my-cases/components/mui/components/CasesTabView.tsx
+++ source/js/my-cases/components/mui/components/CasesTabView.tsx
@@ -6,30 +6,57 @@ import { groupBy } from "../../../../util";
 import CaseView from "./CaseView";
 
 const StatusHintMapping: Record<string, string> = {
-	'approved': 'inprogress',
-	'rejected': 'inprogress',
-	'closed': 'closed',
-	'incomplete': 'todo'
-}
+  approved: "inprogress",
+  rejected: "inprogress",
+  closed: "closed",
+  incomplete: "todo",
+};
 
-const CasesTabView = ({cases}: {cases: Case[]}) => {
-	const {phrase} = useContext(PhraseContext)
+const CasesTabView = ({ cases }: { cases: Case[] }) => {
+  const { phrase } = useContext(PhraseContext);
 
-	const [groupedCases] = useState(groupBy(cases, ({statusHint}) => StatusHintMapping[statusHint || 'approved'] || StatusHintMapping['approved']))
+  const [groupedCases] = useState(
+    groupBy(
+      cases,
+      ({ statusHint }) =>
+        StatusHintMapping[statusHint || "approved"] ||
+        StatusHintMapping["approved"]
+    )
+  );
 
-	const [tabs] = useState([{
-		label: phrase('cases.grouping.todo', 'Att Göra'),
-		tabContent: () => <>{groupedCases['todo']?.map(c => <CaseView data={c} />)}</>
-	}, {
-		label: phrase('cases.grouping.inprogress', 'Pågående'),
-		tabContent: () => <>{groupedCases['inprogress']?.map(c => <CaseView data={c} />)}</>
-	}, {
-		label: phrase('cases.grouping.closed', 'Avslutade'),
-		tabContent: () => <>{groupedCases['closed']?.map(c => <CaseView data={c} />)}</>
-	}])
-	return (
-		<TabView tabs={tabs} />
-	)
-}
+  const [tabs] = useState([
+    {
+      label: phrase("cases.grouping.todo", "Att Göra"),
+      tabContent: () => (
+        <>
+          {groupedCases["todo"]?.map((c) => (
+            <CaseView data={c} />
+          ))}
+        </>
+      ),
+    },
+    {
+      label: phrase("cases.grouping.inprogress", "Pågående"),
+      tabContent: () => (
+        <>
+          {groupedCases["inprogress"]?.map((c) => (
+            <CaseView data={c} />
+          ))}
+        </>
+      ),
+    },
+    {
+      label: phrase("cases.grouping.closed", "Avslutade"),
+      tabContent: () => (
+        <>
+          {groupedCases["closed"]?.map((c) => (
+            <CaseView data={c} />
+          ))}
+        </>
+      ),
+    },
+  ]);
+  return <TabView tabs={tabs} />;
+};
 
-export default CasesTabView
+export default CasesTabView;
