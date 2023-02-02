1836a521c3f98655194823ed521ab43318e28b67
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-02-02 15:24:27 +0100

    formatting

--- source/js/my-cases/components/mui/components/CaseCard.tsx
+++ source/js/my-cases/components/mui/components/CaseCard.tsx
@@ -1,56 +1,59 @@
-import { Button, Card, CardActions, CardContent, Chip, Stack, Typography } from "@mui/material"
-import { CaseAction } from "../../../../about-me-service/AboutMeContext"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { CaseAction } from "../../../../about-me-service/AboutMeContext";
 
 interface CaseCardProps {
-	updateTime: string
-	label?: string
-	description?: string
-	status?: string
-	statusHint?: string
-	actions?: CaseAction[],
-	renderChildren?: () => JSX.Element
+  updateTime: string;
+  label?: string;
+  description?: string;
+  status?: string;
+  statusHint?: string;
+  actions?: CaseAction[];
+  renderChildren?: () => JSX.Element;
 }
 
 const CaseCard = ({
-	props: {
-		updateTime,
-		label,
-		description,
-		status,
-		actions,
-		renderChildren
-	}
-}: {props: CaseCardProps}): JSX.Element => (
-	<Card>
-	<CardContent>
-		<Stack
-			direction="row"
-			justifyContent="space-between"
-			alignItems="flex-start"
-			spacing={2}>
-			<Typography variant="h5" gutterBottom>
-				{label}
-			</Typography>
-			<Typography gutterBottom>
-				<Stack>
-					<Typography>{updateTime}</Typography>
-					<Typography>
-						{status && <Chip label={status}/>}
-					</Typography>
-				</Stack>
-			</Typography>
-		</Stack>
+  props: { updateTime, label, description, status, actions, renderChildren },
+}: {
+  props: CaseCardProps;
+}): JSX.Element => (
+  <Card>
+    <CardContent>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="flex-start"
+        spacing={2}
+      >
+        <Typography variant="h5" gutterBottom>
+          {label}
+        </Typography>
+        <Typography gutterBottom>
+          <Stack>
+            <Typography>{updateTime}</Typography>
+            <Typography>{status && <Chip label={status} />}</Typography>
+          </Stack>
+        </Typography>
+      </Stack>
 
-		<Typography gutterBottom>
-			{description}
-		</Typography>
-		{renderChildren?.()}
-		<CardActions>
-			{actions?.map(({label, url}) => <Button target="_blank" href={url}>{label}</Button>)}
-		</CardActions>
-	</CardContent>
-</Card>
+      <Typography gutterBottom>{description}</Typography>
+      {renderChildren?.()}
+      <CardActions>
+        {actions?.map(({ label, url }) => (
+          <Button target="_blank" href={url}>
+            {label}
+          </Button>
+        ))}
+      </CardActions>
+    </CardContent>
+  </Card>
+);
 
-)
-
-export default CaseCard
+export default CaseCard;
