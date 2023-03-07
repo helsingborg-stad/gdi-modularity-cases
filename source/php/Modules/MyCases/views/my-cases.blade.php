2d91de5ea2ccb1928dd2bd6083c1ded0c8cd3d61
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-03-07 18:56:42 +0100

    add check

--- source/php/Modules/MyCases/views/my-cases.blade.php
+++ source/php/Modules/MyCases/views/my-cases.blade.php
@@ -1,2 +1,2 @@
-<div class="js-gdi-modularity-my-cases" data-about-me-graphql-json={{ $aboutMeGraphQLJson }}
-    data-about-me-graphql-uri={{ $aboutMeGraphQLUri }}></div>
+<div class="js-gdi-modularity-my-cases" data-about-me-graphql-uri={{ $aboutMeGraphQLUri }}
+    @if (!empty($aboutMeGraphQLJson)) data-about-me-graphql-json={{ $aboutMeGraphQLJson }} @endif></div>
